48a255b5f9edab578e5a94d2069dcde842a3233a
reachbikramsingh <reachbikramsingh@gmail.com>
2018-07-06 11:39:03 +0530

    hello new commit with updated code..

---
+++ src/com/bikram/dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.bikram.dao;
+
+import java.util.List;
+
+import com.bikram.model.Employee;
+
+public interface EmployeeDao {
+
+	public void addEmployee(Employee employee);
+
+	public List<Employee> listEmployeess();
+
+	public Employee getEmployee(int empid);
+
+	public void deleteEmployee(Employee employee);
+}
